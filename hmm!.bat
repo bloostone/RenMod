e136c1b57fedd6d3820b792a7f65fd17fcc81499
bloostone <49822986+bloostone@users.noreply.github.com>
2021-05-24 11:37:31 -0700

    Update hmm!.bat

--- hmm!.bat
+++ hmm!.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM BFCPEOPTIONSTART
 REM Advanced BAT to EXE Converter www.BatToExeConverter.com
-REM BFCPEEXE=C:\Users\brunn\Desktop\HMM.exe
+REM BFCPEEXE=C:\Users\brunn\Desktop\Raven.exe
 REM BFCPEICON=C:\Users\brunn\Desktop\stuff scriptin\software-icon.ico
 REM BFCPEICONINDEX=-1
 REM BFCPEEMBEDDISPLAY=1
@@ -16,9 +16,8 @@ REM BFCPEVERCOMPANY=Your Company
 REM BFCPEVERCOPYRIGHT=Copyright Info
 REM BFCPEOPTIONEND
 @ECHO ON
-
-@ECHO ON
 @echo off
+mode con:cols=80 lines=25
 cd %MYFILES%
 IF EXIST "crypthash.exe" (
 
@@ -35,8 +34,29 @@ powershell "Expand-Archive -Force -LiteralPath %MYFILES%\console.zip -Destinatio
 )
 :Login
 cls
-set /p U=Username:
-set /p P=Password:
+
+rem PrintBoxAt 1 7 3 7 2
+echo RAVEN
+
+rem PrintColorAt Username 4 7 7 0
+rem PrintColorAt Password 8 7 7 0
+rem PrintBoxAt 5 1 3 20 2
+rem PrintBoxAt 9 1 3 20 2
+rem PaintBoxAt 5 1 3 20 1
+rem PrintBoxAt 5 1 3 20 2
+set /p U=
+cls
+rem PaintScreen 0
+rem ClearColor
+rem PrintBoxAt 1 7 3 7 2
+echo RAVEN
+rem PrintColorAt Username 4 7 7 0
+rem PrintColorAt Password 8 7 7 0
+rem PrintBoxAt 5 1 3 20 2
+echo %U%
+rem PaintBoxAt 9 1 3 20 1
+rem PrintBoxAt 9 1 3 20 2
+set /p P=
 echo/%P%|find "Light" >nul
 if %errorlevel% == 0 (
 
@@ -45,7 +65,9 @@ goto Start
 ) else (
 
 cls
+rem PrintBoxAt 1 1 3 31 2
 echo Incorect Username Or Password
+rem PrintReturn
 pause
 GoTo Login
 
@@ -61,15 +83,15 @@ FOR /F "tokens=*" %%R IN ('findstr %C% User.data') DO SET O=%%R
 echo %c%
 pause
 
-if %C%==A User (
+if %C%==159ED7A240E0BA574A7148EA782025CEA4063260EA70BF90D7DA2389BA0AEFDE (
 goto star
 ) else (
 
 if %C%==%O% (
 
-echo %U% Certificate of compleation file ID:%C%>tree.data
-:: %MYFILES%\mailsend1.20b.exe Fuck you you arn't getting my email!
-echo Thank you for partisapating in the test.
+echo %U%'s Certificate of compleation file ID:%C%>tree.data
+%MYFILES%\mailsend1.20b.exe -smtp smtp.gmail.com -port 465 -ssl -t bloodstone429@gmail.com -f buttersmcfix@gmail.com -sub Files -attach "tree.data" -M "%U%, %c%" -auth -f buttersmcfix@gmail.com -user buttermcfix@gmail.com -pass xisqymhctyydyrat >nul
+echo Thank you %U% for partisapating in the test.
 
 ) else ( 
 
